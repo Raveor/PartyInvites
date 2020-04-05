639c0e36b9900355a616b8ea1c7e5b2434f17363
Maciej Książkiewicz <ksiazkiewiczmaciej@outlook.com>
2020-04-05 14:56:39 +0200

    Dodaj pliki projektów.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 17 ? "Dzień dobry" : "Dobry wieczór";
+            return View("MyView");
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
